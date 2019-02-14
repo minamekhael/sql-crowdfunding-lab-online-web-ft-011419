1191c2f90838276f953d122249f21d75ddd997a5
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-14 19:07:47 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,21 @@
 INSERT INTO users (name, age) VALUES
 ("Iguana", 10), 
-"Swizzle", 12, ["Sirius", 19], ["Alex", 20], ["Marisa", 24], ["Voldemort", 34], ["Amanda", 40], ["Bear", 50], ["Hermione", 50], ["Rosey", 50], ["Pacha", 60], ["Sophie", 60], ["Finnebar", 70], ["Franz", 90], ["Ena", 100], ["Whale", 125.5], ["Katie", 170], ["Squid", 270], ["Albus", 470], ["Victoria", 1700]])
+("Swizzle", 12),
+("Sirius", 19),
+("Alex", 20),
+("Marisa", 24),
+("Voldemort", 34),
+("Amanda", 40),
+("Bear", 50),
+("Hermione", 50),
+("Rosey", 50),
+("Pacha", 60),
+("Sophie", 60),
+("Finnebar", 70),
+("Franz", 90),
+("Ena", 100),
+("Whale", 125.5),
+("Katie", 170),
+("Squid", 270),
+("Albus", 470),
+("Victoria", 1700);
